95da142d6df72ced81fe6e503f05c3abd99948ca
rondDev <contact@rond.cc>
2025-05-01 14:48:23 +0200

    feat: add simple login

--- server/api/login.ts
+++ server/api/login.ts
@@ -1,12 +1,17 @@
 import { db } from '../utils/db.ts';
+import * as argon2 from 'argon2';
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  const user = await db.selectFrom('users').where('username', '=', body.username).executeTakeFirst();
-  if (user) {
-    console.log(user);
-  } else {
-    console.log('No user');
+  const user = await db.selectFrom('users').selectAll().where('username', '=', body.username).executeTakeFirst();
+  if (!user) {
+    console.log('user  doesnt exist');
+    return { error: 'User does not exist' };
   }
+  const match = await argon2.verify(user.password, body.password);
+  if (match) {
+    return { response: 'Signed in!' };
+  }
+  return { error: 'Data entered did not match any records' };
 });
