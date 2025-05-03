01c2d09c56eff37cc5962c3894fec06ea5edfc24
rondDev <contact@rond.cc>
2025-05-03 03:05:35 +0200

    fix: update register page

--- pages/register.vue
+++ pages/register.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 // biome-ignore lint/correctness/noUnusedVariables: Used in form
-function register() {
+async function register() {
   // biome-ignore lint/correctness/noUndeclaredVariables: Auto-imported by nuxt
-  $fetch('/api/register', { method: 'POST', body: credentials });
+  const { response, error } = await $fetch('/api/register', { method: 'POST', body: credentials });
+  if (response) {
+    responseData.data = response;
+  }
+  if (error) {
+    responseData.data = `Error occured: ${error}`;
+  }
 }
 // biome-ignore lint/correctness/noUndeclaredVariables: Auto-imported by nuxt
+const responseData = reactive({
+  data: '',
+});
 const credentials = reactive({
   username: '',
   password: '',
@@ -20,5 +29,8 @@ const credentials = reactive({
       <input id="password" name="password" v-model="credentials.password" type="password" placeholder="Password" />
       <button type="submit">Submit</button>
     </form>
+    <div v-if="responseData.data">
+      <p>{{responseData.data}}</p>
+    </div>
   </div>
 </template>
