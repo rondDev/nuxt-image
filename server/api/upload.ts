70f6b371bbd7ccc67909a27da7d5d8a923cdb3d0
rondDev <contact@rond.cc>
2025-05-03 15:18:44 +0200

    fix: add mimetype and fix formatting

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -4,110 +4,112 @@ import cryptoRandomString from 'crypto-random-string';
 import { ContentFileType } from '@nuxt/content';
 import { db } from '../utils/db';
 import { createId } from '../utils/cuid';
+import mime from 'mime-types';
 
 export default defineEventHandler(async (event) => {
-  try {
-    const formDataBody = await readMultipartFormData(event);
-    if (!formDataBody) {
-      return {
-        error: 'No data provided',
-      };
-    }
-    let fileObject: {
-      name: string;
-      filename: string | undefined;
-      type: string | undefined;
-      data: Buffer;
-    };
-    let uploadKey = '';
-    let randomizeFilename = false;
-    for (const item of formDataBody) {
-      if (item.name === 'd') {
-        fileObject = {
-          name: item.name,
-          filename: item.filename,
-          type: item.type,
-          data: item.data,
-        };
-        console.log(item);
-      } else if (item.name === 'key') {
-        uploadKey = item.data.toString();
-      } else if (item.name === 'randomize_filename') {
-        randomizeFilename = item.data.toString() === 'true';
-      }
-    }
-    if (!uploadKey) {
-      setResponseStatus(event, 401);
-      return {
-        error: 'Unauthorized',
-      };
-    }
-    if (!fileObject) {
-      return {
-        error: "Couldn't parse a file object or no files were provided",
-      };
-    }
+	try {
+		const formDataBody = await readMultipartFormData(event);
+		if (!formDataBody) {
+			return {
+				error: 'No data provided',
+			};
+		}
+		let fileObject: {
+			name: string;
+			filename: string | undefined;
+			type: string | undefined;
+			data: Buffer;
+		};
+		let uploadKey = '';
+		let randomizeFilename = false;
+		for (const item of formDataBody) {
+			if (item.name === 'd') {
+				fileObject = {
+					name: item.name,
+					filename: item.filename,
+					type: item.type,
+					data: item.data,
+				};
+				console.log(item);
+			} else if (item.name === 'key') {
+				uploadKey = item.data.toString();
+			} else if (item.name === 'randomize_filename') {
+				randomizeFilename = item.data.toString() === 'true';
+			}
+		}
+		if (!uploadKey) {
+			setResponseStatus(event, 401);
+			return {
+				error: 'Unauthorized',
+			};
+		}
+		if (!fileObject) {
+			return {
+				error: "Couldn't parse a file object or no files were provided",
+			};
+		}
 
-    const user = await db
-      .selectFrom('users')
-      .innerJoin('file_upload_keys as file', 'file.userId', 'users.id')
-      .where('file.uploadKey', '=', uploadKey)
-      .select(['users.id', 'username'])
-      .executeTakeFirst();
+		const user = await db
+			.selectFrom('users')
+			.innerJoin('file_upload_keys as file', 'file.userId', 'users.id')
+			.where('file.uploadKey', '=', uploadKey)
+			.select(['users.id', 'username'])
+			.executeTakeFirst();
 
-    if (!user) {
-      console.log('Upload key invalid');
-      return {
-        error: 'Upload key invalid',
-      };
-    }
+		if (!user) {
+			console.log('Upload key invalid');
+			return {
+				error: 'Upload key invalid',
+			};
+		}
 
-    // console.log("file", fileObject)
-    // console.log("uploadKey", uploadKey?.data.toString())
-    // console.log("randomizeFilename", randomizeFilename)
+		// console.log("file", fileObject)
+		// console.log("uploadKey", uploadKey?.data.toString())
+		// console.log("randomizeFilename", randomizeFilename)
 
-    const fileNameRand = cryptoRandomString({
-      length: 12,
-      type: 'alphanumeric',
-    });
-    // NOTE: Might need to add file extension to the random name
-    const fileName = randomizeFilename ? fileNameRand : fileObject.filename;
-    const fileKey = `${user.username}/${fileName}`;
+		const fileNameRand = cryptoRandomString({
+			length: 12,
+			type: 'alphanumeric',
+		});
+		// NOTE: Might need to add file extension to the random name
+		const fileName = randomizeFilename ? fileNameRand : fileObject.filename;
+		const fileKey = `${user.username}/${fileName}`;
 
-    const file = await s3Client.send(
-      new PutObjectCommand({
-        Bucket: 'image-uploader',
-        Key: fileKey,
-        ContentType: fileObject.type,
-        Body: fileObject.data,
-      }),
-    );
-    await db
-      .insertInto('files')
-      .values({
-        id: createId(),
-        fileName: fileNameRand,
-        fileSize: bytesToSize(Buffer.byteLength(fileObject.data)),
-        bucket: 'image-uploader',
-        key: fileKey,
-        userId: user.id,
-      })
-      .execute();
-    return {
-      url: `https://i.rond.cc/${fileNameRand}`,
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      error: e,
-    };
-  }
+		const file = await s3Client.send(
+			new PutObjectCommand({
+				Bucket: 'image-uploader',
+				Key: fileKey,
+				ContentType: fileObject.type,
+				Body: fileObject.data,
+			}),
+		);
+		await db
+			.insertInto('files')
+			.values({
+				id: createId(),
+				fileName: fileNameRand,
+				fileSize: bytesToSize(Buffer.byteLength(fileObject.data)),
+				mimeType: mime.lookup(fileObject.filename || ''),
+				bucket: 'image-uploader',
+				key: fileKey,
+				userId: user.id,
+			})
+			.execute();
+		return {
+			url: `https://i.rond.cc/${fileNameRand}`,
+		};
+	} catch (e) {
+		console.error(e);
+		return {
+			error: e,
+		};
+	}
 });
 
 function bytesToSize(bytes: number) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return 'n/a';
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  if (i === 0) return `${bytes} ${sizes[i]}`;
-  return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
+	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+	if (bytes === 0) return 'n/a';
+	const i = Math.floor(Math.log(bytes) / Math.log(1024));
+	if (i === 0) return `${bytes} ${sizes[i]}`;
+	return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
 }
