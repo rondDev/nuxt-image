69fedd2995ad7a5610200ddda2165849791f407f
rondDev <contact@rond.cc>
2025-05-10 10:28:24 +0200

    fix: change background and text colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute min-h-[100vh] min-w-[100vw] bg-[#0a000a]">
+  <div class="absolute min-h-[100vh] min-w-[100vw] bg-[#09090b] text-[#fafafa]">
     <slot />
   </div>
 </template>
