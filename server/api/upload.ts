25b6b6330821b65e4253697ff07df55005f31727
rondDev <contact@rond.cc>
2025-05-11 07:55:16 +0200

    fix: move bytesToSize to utils

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -105,11 +105,3 @@ export default defineEventHandler(async (event) => {
 		};
 	}
 });
-
-function bytesToSize(bytes: number) {
-	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-	if (bytes === 0) return 'n/a';
-	const i = Math.floor(Math.log(bytes) / Math.log(1024));
-	if (i === 0) return `${bytes} ${sizes[i]}`;
-	return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
-}
