ddedf04186bf85328ad9187e6a4bf4b860e453ab
rondDev <contact@rond.cc>
2025-05-11 09:18:55 +0200

    fix: add homepage_card css shortcut

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig, presetWind4 } from 'unocss';
 
 export default defineConfig({
-  presets: [presetWind4({ reset: true })],
-  rules: [],
-  shortcuts: [
-    {
-      menuBarButton:
-        ' cursor-pointer transform transition ease-in-out hover:scale-[1.1] hover:translate-y-[-3px] hover:rotate-z-10 hover:color-[#f966abFF] motion-reduce:transition-none motion-reduce:hover:transform-none',
-    },
-  ],
+	presets: [presetWind4({ reset: true })],
+	rules: [],
+	shortcuts: [
+		{
+			menuBarButton:
+				' cursor-pointer transform transition ease-in-out hover:scale-[1.1] hover:translate-y-[-3px] hover:rotate-z-10 hover:color-[#f966abFF] motion-reduce:transition-none motion-reduce:hover:transform-none',
+			homepage_card:
+				'rounded-2xl w-[12rem] h-[12rem] bg-[#0f172b4b] border-[3px] border-[#0414384b] shadow-sm shadow-purple-700 flex flex-col p-3',
+		},
+	],
 });
