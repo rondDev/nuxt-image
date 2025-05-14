6fbb7a28f9469d275e8399b06b9949adfbed7a67
rondDev <contact@rond.cc>
2025-05-14 05:58:20 +0200

    fix: invert logic to avoid return in middleware

--- server/api/verify-session.ts
+++ server/api/verify-session.ts
@@ -3,36 +3,32 @@ import adze from 'adze';
 export default defineEventHandler(async (event) => {
   try {
     const c = getCookie(event, 'session');
-    if (!c) {
+    if (c) {
+      const s = await db
+        .selectFrom('sessions')
+        .where('sessionToken', '=', c)
+        .select(['userId'])
+        .executeTakeFirstOrThrow();
+
+      const u = await db
+        .selectFrom('users')
+        .innerJoin('file_upload_keys', 'users.id', 'file_upload_keys.userId')
+        .where('users.id', '=', s.userId)
+        .select([
+          'users.id',
+          'users.createdAt',
+          'users.updatedAt',
+          'users.username',
+          'users.email',
+          'users.prefs',
+          'file_upload_keys.uploadKey',
+        ])
+        .executeTakeFirstOrThrow();
       return {
-        user: undefined,
-        valid: false,
+        user: u,
+        valid: !!u,
       };
     }
-    const s = await db
-      .selectFrom('sessions')
-      .where('sessionToken', '=', c)
-      .select(['userId'])
-      .executeTakeFirstOrThrow();
-
-    const u = await db
-      .selectFrom('users')
-      .innerJoin('file_upload_keys', 'users.id', 'file_upload_keys.userId')
-      .where('users.id', '=', s.userId)
-      .select([
-        'users.id',
-        'users.createdAt',
-        'users.updatedAt',
-        'users.username',
-        'users.email',
-        'users.prefs',
-        'file_upload_keys.uploadKey',
-      ])
-      .executeTakeFirstOrThrow();
-    return {
-      user: u,
-      valid: !!u,
-    };
   } catch (e) {
     adze.error('[verify-session.ts | catch]', e);
   }
