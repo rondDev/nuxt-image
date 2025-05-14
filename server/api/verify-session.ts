1dbe91a9d87f828888e555d59ef5ef683d59115b
rondDev <contact@rond.cc>
2025-05-14 05:22:53 +0200

    fix: actually check session

--- server/api/verify-session.ts
+++ server/api/verify-session.ts
@@ -1,26 +1,36 @@
 import adze from 'adze';
 
 export default defineEventHandler(async (event) => {
-	try {
-		const c = getCookie(event, 'session');
-		const u = await db
-			.selectFrom('users')
-			.innerJoin('file_upload_keys', 'users.id', 'file_upload_keys.userId')
-			.select([
-				'users.id',
-				'users.createdAt',
-				'users.updatedAt',
-				'users.username',
-				'users.email',
-				'users.prefs',
-				'file_upload_keys.uploadKey',
-			])
-			.executeTakeFirstOrThrow();
-		return {
-			user: u,
-			valid: !!u,
-		};
-	} catch (e) {
-		adze.error('[verify-session.ts | catch]', e);
-	}
+  try {
+    const c = getCookie(event, 'session');
+    if (!c) {
+      return;
+    }
+    const s = await db
+      .selectFrom('sessions')
+      .where('sessionToken', '=', c)
+      .select(['userId'])
+      .executeTakeFirstOrThrow();
+
+    const u = await db
+      .selectFrom('users')
+      .innerJoin('file_upload_keys', 'users.id', 'file_upload_keys.userId')
+      .where('users.id', '=', s.userId)
+      .select([
+        'users.id',
+        'users.createdAt',
+        'users.updatedAt',
+        'users.username',
+        'users.email',
+        'users.prefs',
+        'file_upload_keys.uploadKey',
+      ])
+      .executeTakeFirstOrThrow();
+    return {
+      user: u,
+      valid: !!u,
+    };
+  } catch (e) {
+    adze.error('[verify-session.ts | catch]', e);
+  }
 });
