2c55ff18545041508790e011c3f9536eac441bf2
rondDev <contact@rond.cc>
2025-05-14 05:11:12 +0200

    fix: try to set contentype of response

--- server/api/file/[filename].ts
+++ server/api/file/[filename].ts
@@ -3,41 +3,44 @@ import { db } from '../../utils/db';
 import { s3Client } from '../../utils/s3';
 
 export default defineEventHandler(async (event) => {
-	try {
-		const fileUrl = getRouterParam(event, 'filename');
-		if (!fileUrl) {
-			return {
-				error: 'No file url specified',
-			};
-		}
-		const fileData = await db
-			.selectFrom('files')
-			.where('fileName', '=', fileUrl)
-			.selectAll()
-			.executeTakeFirst();
+  try {
+    const fileUrl = getRouterParam(event, 'filename');
+    if (!fileUrl) {
+      return {
+        error: 'No file url specified',
+      };
+    }
+    const fileData = await db
+      .selectFrom('files')
+      .where('fileName', '=', fileUrl)
+      .selectAll()
+      .executeTakeFirst();
 
-		if (!fileData) {
-			return {
-				error: 'File not found',
-			};
-		}
+    if (!fileData) {
+      return {
+        error: 'File not found',
+      };
+    }
 
-		const file = await s3Client.send(
-			new GetObjectCommand({
-				Bucket: fileData.bucket,
-				Key: fileData.key,
-			}),
-		);
-		const s = file.Body?.transformToWebStream();
-		if (!s) {
-			return {
-				error: 'No file found',
-			};
-		}
-		return s;
-	} catch (e) {
-		return {
-			error: e,
-		};
-	}
+    const file = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: fileData.bucket,
+        Key: fileData.key,
+      }),
+    );
+    const s = file.Body?.transformToWebStream();
+    if (!s) {
+      return {
+        error: 'No file found',
+      };
+    }
+    setResponseHeaders(event, {
+      'Content-Type': file.ContentType,
+    });
+    return s;
+  } catch (e) {
+    return {
+      error: e,
+    };
+  }
 });
