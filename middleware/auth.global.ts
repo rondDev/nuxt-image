4b6d3ae3275a9810f4ba23f511928c1b3ec5447b
rondDev <contact@rond.cc>
2025-05-14 05:48:05 +0200

    fix: check if valid

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,32 +2,32 @@ import { useUserStore } from '~/stores/user';
 import adze from 'adze';
 
 export default async function defineNuxtRouteMiddleware() {
-	if (import.meta.client) return;
-	const user = useUserStore();
-	try {
-		const session = useCookie('session').value;
-		if (!session) {
-			throw new Error('No user session');
-		}
+  if (import.meta.client) return;
+  const user = useUserStore();
+  try {
+    const session = useCookie('session').value;
+    if (!session) {
+      throw new Error('No user session');
+    }
 
-		const res = await $fetch('/api/verify-session');
-		if (!res) {
-			return {
-				error: 'No user',
-			};
-		}
-		const u = res.user;
-		user.$patch({
-			id: u.id,
-			createdAt: u.createdAt,
-			updatedAt: u.updatedAt,
-			username: u.username,
-			email: u.email,
-			prefs: u.prefs,
-			uploadKey: u.uploadKey,
-		});
-		adze.info('[middleware | user.$state]', user.$state);
-	} catch (e) {
-		adze.error('[auth.global.ts | catch]', e);
-	}
+    const res = await $fetch('/api/verify-session');
+    if (!res || !res.valid) {
+      return {
+        error: 'No user',
+      };
+    }
+    const u = res.user;
+    user.$patch({
+      id: u.id,
+      createdAt: u.createdAt,
+      updatedAt: u.updatedAt,
+      username: u.username,
+      email: u.email,
+      prefs: u.prefs,
+      uploadKey: u.uploadKey,
+    });
+    adze.info('[middleware | user.$state]', user.$state);
+  } catch (e) {
+    adze.error('[auth.global.ts | catch]', e);
+  }
 }
